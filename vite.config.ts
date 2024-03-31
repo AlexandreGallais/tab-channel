a099dcdab295afa47f401be82df32f6f0f6fe117
Alexandre GALLAIS <37043327+AlexandreGallais@users.noreply.github.com>
2024-03-31 13:34:22 +0200

    update unit test config

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,13 @@ export default defineConfig({
             enabled: true,
             provider: 'istanbul',
             include: ['src'],
+            all: true,
+            thresholds: {
+                lines: 100,
+                branches: 100,
+                functions: 100,
+                statements: 100,
+            },
         },
     },
 });
