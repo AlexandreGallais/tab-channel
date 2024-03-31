a3b170d2d17d757aea655c7f8a35125371431404
Alexandre GALLAIS <37043327+AlexandreGallais@users.noreply.github.com>
2024-03-31 14:49:39 +0200

    v1.0.0

--- README.md
+++ README.md
@@ -1,3 +1,83 @@
 # TabChannel
 
 A simple class for communicating between different tabs on the same domain using BroadcastChannel.
+
+## Installation
+
+Follow this video to install the npm package.
+
+<https://youtu.be/7CNC0QBCY-Y?si=5yk7pI5w1X5hKMoQ&t=250>
+
+Create a `.npmrc` file
+
+```bash
+@alexandregallais:registry=https://npm.pkg.github.com
+//npm.pkg.github.com/:_authToken=YOUR_TOKEN
+```
+
+Follow the video to create your token (**YOUR TOKEN MUST BE KEPT SECRET**).
+
+Then, install the package with npm.
+
+```bash
+npm i @alexandregallais/tab-channel
+```
+
+## Usage
+
+```ts
+import { TabChannel } from '@alexandregallais/tab-channel';
+
+const tabChannel = new TabChannel<string>('my-channel-id');
+
+tabChannel.onData((data) => {
+    console.log(data);
+    // logs 'Hello!' if you open a new tab.
+});
+
+tabChannel.postData('Hello!');
+```
+
+### Listen once
+
+You can listen to the event just once with the `once` option.
+
+```ts
+import { TabChannel } from '@alexandregallais/tab-channel';
+
+const tabChannel = new TabChannel<string>('my-channel-id');
+
+tabChannel.onData(
+    (data) => {
+        console.log(data);
+        // logs 'Hello!' if you open a new tab.
+    },
+    { once: true },
+);
+
+tabChannel.onData((data) => {
+    console.log(data);
+    // logs 'Hello!' if you open a new tab.
+    // logs 'Hello!' if you open a new tab.
+});
+
+tabChannel.postData('Hello!');
+tabChannel.postData('Hello!');
+```
+
+### Emit to its own tab
+
+Send the event to its own tab and the others with the option `self`.
+
+```ts
+import { TabChannel } from '@alexandregallais/tab-channel';
+
+const tabChannel = new TabChannel<string>('my-channel-id');
+
+tabChannel.onData((data) => {
+    console.log(data);
+    // logs 'Hello!'
+});
+
+tabChannel.postData('Hello!', { self: true });
+```
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 interface ListenerOptions {
     /**
-     * A boolean value indicating that the `listener` should be invoked at most once after being added.\
-     * If `true`, the `listener` would be automatically removed when invoked. If not specified, defaults to `false`.
+     * A boolean value indicating whether the listener should be invoked at most once after being added.\
+     * If `true`, the listener would be automatically removed when invoked. If not specified, defaults to `false`.
      */
     once?: boolean;
 }
 
 interface EmitterOptions {
     /**
-     * A boolean value indicating whether the emitted message should also be sent to the `listeners` of the same tab.\
-     * If `true`, the `listeners` of this Tab will be invoked after a message has been emitted. If not specified, defaults to `false`.
+     * A boolean value indicating whether the emitted message should also be sent to the listeners of the same tab.\
+     * If `true`, the listeners of this Tab will be invoked after a message has been emitted. If not specified, defaults to `false`.
      */
     self?: boolean;
 }
@@ -28,19 +28,13 @@ type Store<T> = {
 };
 
 export class TabChannel<T> {
-    /**
-     * The BroadcastChannel used for communication.
-     */
     private channel: BroadcastChannel;
-
-    /**
-     * Storage for event listeners.
-     */
     private store: Store<T> = { once: [], default: [] };
 
     /**
      * Retrieves or generates a unique identifier for the tab.\
      * If an identifier is already stored in sessionStorage, it retrieves it, otherwise it generates one.
+     * @returns The tab identifier.
      */
     private get tabId(): string {
         let id = sessionStorage.getItem('tab-channel-id');
@@ -54,7 +48,7 @@ export class TabChannel<T> {
 
     /**
      * Emits the provided data to all registered listeners.
-     * @param data The data to emit.
+     * @param data - The data to emit.
      */
     private emit = (data: T) => {
         this.store.default.forEach((x) => x(data));
@@ -64,15 +58,9 @@ export class TabChannel<T> {
 
     /**
      * Creates a new TabChannel instance with the specified name.
-     *
-     * @description
-     * This constructor creates a new `TabChannel` instance that utilizes the `BroadcastChannel` interface for communication between tabs.\
-     * The provided name will be used as the name of the `BroadcastChannel`.\
-     * Please be cautious when choosing a name to avoid conflicts with other `BroadcastChannel`.
-     *
+     * @param name - The name of the channel. The provided name will be used as the name of the `BroadcastChannel`.
+     * @remarks Please be cautious when choosing a name to avoid conflicts with other BroadcastChannels.
      * @see https://developer.mozilla.org/en-US/docs/Web/API/BroadcastChannel
-     *
-     * @param name The name of the channel.
      */
     constructor(name: string) {
         this.channel = new BroadcastChannel(name);
@@ -91,8 +79,8 @@ export class TabChannel<T> {
 
     /**
      * Sets up a callback function to handle incoming data posted to the channel.
-     * @param listener A callback function invoked when data is posted.
-     * @param options Optional parameters that control the behavior of the listener function.
+     * @param listener - A callback function invoked when data is posted.
+     * @param options - Optional parameters that control the behavior of the listener function.
      */
     onData = (listener: Listener<T>, options?: ListenerOptions) => {
         this.store[options?.once ? 'once' : 'default'].push(listener);
@@ -100,8 +88,8 @@ export class TabChannel<T> {
 
     /**
      * Posts data to the channel.
-     * @param data The data to be sent.
-     * @param options Optional parameters that specify additional behavior for the postData function.
+     * @param data - The data to be sent.
+     * @param options - Optional parameters that specify additional behavior for the postData function.
      */
     postData = (data: T, options?: EmitterOptions) => {
         this.channel.postMessage({
